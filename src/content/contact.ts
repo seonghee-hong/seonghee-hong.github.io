f52f6c8d78d90b59480ccbadd65a702760fdb97e
shong <sh.hong580@gmail.com>
2026-07-26 22:10:15 -0400

    Update contact.ts

--- src/content/contact.ts
+++ src/content/contact.ts
@@ -1,6 +1,6 @@
 export const contact = {
-  name: 'Jina Lee',
-  title: 'Assistant Professor of Sociology',
-  affiliation: 'University of Illinois at Urbana-Champaign',
+  name: 'Seongee Hong',
+  title: 'Assistant Professor of Insturction of Human Development and Family Science',
+  affiliation: 'Ohio University',
   note: 'I welcome inquiries from potential collaborators and graduate students with interests in gender, evaluation, science of science, or cultural sociology.',
 }
